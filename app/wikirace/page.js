1047e30601d497f629477248e71356189dd18afd
chrysalis-iiserb <chrysalis@iiserb.ac.in>
2026-03-22 01:33:57 +0530

    Create page.js

---
+++ app/wikirace/page.js
@@ -0,0 +1,15 @@
+export default function WikiRace() {
+  return (
+    <iframe
+      src="/wiki_racing.html"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+    />
+  );
+}
+```
+
+---
+
+Then visit:
+```
+https://chrysalis-iiserb.vercel.app/wikirace
