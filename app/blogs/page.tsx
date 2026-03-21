126c122e6c4867297c24483837231ce2f64044d2
chrysalis-iiserb <chrysalis@iiserb.ac.in>
2026-03-22 01:58:56 +0530

    Update page.tsx

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,54 +1,32 @@
-
-// import React, { useEffect, useState } from 'react'
-
 import BlogCard from "@/components/BlogCard";
-import { Client } from "@notionhq/client";
-import NotionBlocks from "notion-block-renderer";
-import { json } from "stream/consumers";
-
-
-
-
 
 const fetchfromNotion = async() => {
+  try {
     const res = await fetch("https://chrysalis-weld.vercel.app/api/blogsHolder", {next: {revalidate: 1}});
+    if (!res.ok) return [];
     const data = await res.json();
-    return data.data
+    return data.data || [];
+  } catch(e) {
+    return [];
   }
-
+}
 
 const Blogs = async() => {
-
-
-
-    const data = await fetchfromNotion();
-
-
-
+  const data = await fetchfromNotion();
   return (
     <div className="w-full min-h-screen flex justify-center">
       <div className="md:w-[60%] border h-full p-12">
-
         <h1 className="text-center text-4xl py-6 capitalize font-bold">Find out awesome Blogs here!</h1>
-
-      
         {data && data.map((blogCard:any) => {
           return(
             <div key={blogCard.id}>
-                <BlogCard blogCard={blogCard} />
+              <BlogCard blogCard={blogCard} />
             </div>
           )
         })}
-
-
-</div>
-
-
-
+      </div>
     </div>
-
   )
 }
-
 
 export default Blogs;
