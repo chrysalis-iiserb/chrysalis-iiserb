39e8e7baaf2df9cb20f6c557942bcc1432215519
chrysalis-iiserb <chrysalis@iiserb.ac.in>
2026-03-22 01:08:50 +0530

    Create wikirace.js

---
+++ public/wikirace.js
@@ -0,0 +1,16 @@
+export default function WikiRace() {
+  return (
+    <iframe
+      src="/wikirace_3_.html"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+    />
+  );
+}
+```
+5. Click **"Commit changes"**
+
+---
+
+### Step 4: Wait 1–2 minutes, then visit:
+```
+https://chrysalis-iiserb.vercel.app/wikirace
